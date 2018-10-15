62a0e44064b2efd10e73c7e61d1d7564eb4ba86c
Przemysław Lenart <przemek.lenart@gmail.com>
2018-10-15 23:04:22 +0200

    Add comment to sdf lib

--- libs/sdf/src/lib.rs
+++ libs/sdf/src/lib.rs
@@ -1,3 +1,12 @@
+/*!
+**Multi Channel Signed Distance Field renderer library.**
+
+It allows you to create multi channel signed distance fields rendered
+to the memory. Currently rendering from fonts is supported as well.
+*/
+
+#![warn(missing_docs)]
+
 extern crate cgmath;
 extern crate rusttype;
 
